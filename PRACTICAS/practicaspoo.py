3161fdee547627f3862b74c2b9fc6973eda14fc1
Samuel Fernandez <samueve24@gmail.com>
2025-07-29 16:21:06 -0300

    carpetas-de-practica

---
+++ PRACTICAS/practicaspoo.py
@@ -0,0 +1,14 @@
+#PRACTICAS POO 
+
+class Mapa:
+    def __init__(self,filas,columnas):
+        self.filas = filas
+        self.columnas = columnas
+        self.mapa = [[0 for _ in range(columnas)]for _ in range(filas)]
+    
+    def colocar_obstaculo(self)):
+        
+        
+
+
+
